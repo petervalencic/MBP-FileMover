7cc240b1ec255415cf616c793458563076f72531
Peter Valencic <peter.valencic@gmail.com>
2018-11-10 19:48:24 +0100

    Skripta se bo izvajala s pomočjo Cron-a zato ne potrebujem thread-a

--- src/org/mbp/FileMover.java
+++ src/org/mbp/FileMover.java
@@ -21,76 +21,58 @@ import org.w3c.dom.NodeList;
 
 public class FileMover {
 
-    /**
-     *
-     * @param args
-     */
-    private void runIt(String args[]) {
-
-        System.out.println("Začasna datoteka: " + args[0]);
-        System.out.println("Datoteka katero prebira FFMPEG: " + args[1]);
-        System.out.println("Naslov merilne naprave: " + args[2]);
-        System.out.println("===================================");
-
-        Timer timer = new Timer();
-
-        timer.schedule(new TimerTask() {
-            public void run() {
-
-                DocumentBuilderFactory dbf;
-                DocumentBuilder db;
-                Document doc;
-                XPathFactory xpathFactory;
-
-                String temperatura;
-                String slanost;
-
-                try {
-
-                    dbf = DocumentBuilderFactory.newInstance();
-                    db = dbf.newDocumentBuilder();
-                    doc = db.parse(new URL(args[1]).openStream());
-                    xpathFactory = XPathFactory.newInstance();
-                    XPath xpath = xpathFactory.newXPath();
-
-                    XPathExpression expr = xpath.compile("/root/temp/value/text()");
-                    NodeList nodes = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
-                    temperatura = nodes.item(0).getNodeValue().trim();
-
-                    expr = xpath.compile("/root/sal/value/text()");
-                    nodes = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
-                    slanost = nodes.item(0).getNodeValue().trim();
-
-                    PrintWriter writer = new PrintWriter(args[0], "UTF-8");
-                    writer.println("T: " + temperatura + " °C");
-                    writer.println("S: " + slanost + " PSU");
-                    writer.close();
-
-                    Path sourceFile = Paths.get(args[0]);
-                    Path destinationFile = Paths.get(args[1]);
-                    Files.move(sourceFile, destinationFile, StandardCopyOption.ATOMIC_MOVE);
-
-                    System.out.println("move.." + new Date().toString());
-                    System.out.println(args[0]);
-                    System.out.println(args[1]);
-                    System.out.println("==================");
-                } catch (ArrayIndexOutOfBoundsException ex) {
-                    System.out.println("Preveri število argumentov");
-                    System.out.println("arg[0] = naziv začasne datoteke npr. c:/video/temppodatki.txt");
-                    System.out.println("arg[1] = naziv datoteke iz katere bere FFMPEG npr. c:/video/podatki.txt");
-                    System.out.println("arg[2] = IP naslov merilne enote od koder se pridobi XML response");
-                    ex.printStackTrace();
-                    System.exit(1);
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-
-            }
-        }, 0, 500);
-    }
+   
 
     public static void main(String[] args) {
-        new FileMover().runIt(args);
+        DocumentBuilderFactory dbf;
+        DocumentBuilder db;
+        Document doc;
+        XPathFactory xpathFactory;
+
+        String temperatura;
+        String slanost;
+
+        try {
+
+            dbf = DocumentBuilderFactory.newInstance();
+            db = dbf.newDocumentBuilder();
+            doc = db.parse(new URL(args[1]).openStream());
+            xpathFactory = XPathFactory.newInstance();
+            XPath xpath = xpathFactory.newXPath();
+
+            XPathExpression expr = xpath.compile("/root/temp/value/text()");
+            NodeList nodes = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+            temperatura = nodes.item(0).getNodeValue().trim();
+
+            expr = xpath.compile("/root/sal/value/text()");
+            nodes = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+            slanost = nodes.item(0).getNodeValue().trim();
+
+            PrintWriter writer = new PrintWriter(args[0], "UTF-8");
+            writer.println("T: " + temperatura + " °C");
+            writer.println("S: " + slanost + " PSU");
+            writer.close();
+
+            Path sourceFile = Paths.get(args[0]);
+            Path destinationFile = Paths.get(args[1]);
+            Files.move(sourceFile, destinationFile, StandardCopyOption.ATOMIC_MOVE);
+
+            /*
+            System.out.println("move.." + new Date().toString());
+            System.out.println(args[0]);
+            System.out.println(args[1]);
+            System.out.println("==================");
+             */
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Preveri število argumentov");
+            System.out.println("arg[0] = naziv začasne datoteke npr. c:/video/temppodatki.txt");
+            System.out.println("arg[1] = naziv datoteke iz katere bere FFMPEG npr. c:/video/podatki.txt");
+            System.out.println("arg[2] = IP naslov merilne enote od koder se pridobi XML response");
+            ex.printStackTrace();
+            System.exit(1);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
 }
